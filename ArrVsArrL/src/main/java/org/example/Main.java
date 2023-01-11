29135f370229c1a2091915162731b952b90736c8
kamath7 <adithyakamath96@gmail.com>
2023-01-11 12:37:35 +0530

    basic array

---
+++ ArrVsArrL/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] friends = new String[10];
+        //Array always fixed size
+
+        String[] indianDishes = {"Palak Paneer", "Jeera Rice","Rasmalai","Garlic Naan"};
+
+        ArrayList<String> mangaloreanDishes = new ArrayList<>();
+    }
+}
