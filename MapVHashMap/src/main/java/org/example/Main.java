2d2aafc644a8b97df73f45d2f4983c2c1a6af3a3
kamath7 <adithyakamath96@gmail.com>
2023-01-12 22:52:59 +0530

    simple hashmap

---
+++ MapVHashMap/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> employees = new HashMap<>();
+
+        employees.put(1,"Michael");
+        employees.put(2,"Dwight");
+        employees.put(3,"Jim");
+        employees.put(4,"David");
+
+        System.out.println(employees);
+
+    }
+}
