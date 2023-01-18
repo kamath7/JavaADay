ae89380d28aaaf1b8e6363568fb82b48824e6495
kamath7 <adithyakamath96@gmail.com>
2023-01-18 18:03:42 +0530

    using the thread class done

--- MultiThreading/src/main/java/org/example/Main.java
+++ MultiThreading/src/main/java/org/example/Main.java
@@ -3,9 +3,12 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        MultiThreader multiThreader = new MultiThreader();
+        MultiThreader multiThreader = new MultiThreader(1);
+        MultiThreader multiThreader1 = new MultiThreader(2);
+        MultiThreader multiThreader12 = new MultiThreader(3);
+
         multiThreader.start();
-        MultiThreader multiThreader1 = new MultiThreader();
         multiThreader1.start();
+        multiThreader12.start();
     }
 }
--- MultiThreading/src/main/java/org/example/MultiThreader.java
+++ MultiThreading/src/main/java/org/example/MultiThreader.java
@@ -1,12 +1,18 @@
 package org.example;
 
 public class MultiThreader extends Thread  {
+    private int threadNum;
+    public MultiThreader(int threadNum){
+        this.threadNum = threadNum;
+    }
     @Override
     public void run() {
         for (int i = 1; i <=5 ; i++){
 
-            System.out.println(i);
-
+            System.out.println("From Thread"+threadNum+"->"+i);
+            if (threadNum == 3){
+                throw new RuntimeException("NO!");
+            }
             try {
                 Thread.sleep(10);
             }catch(Exception e){
